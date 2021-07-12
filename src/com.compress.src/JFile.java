402f33449452f40554e564e44c6a97441f1368df
cr0wchen <cr0wchen@qq.com>
2021-07-12 15:50:56 +0800

    重写File类中的toString方法和排序接口

---
+++ src/com.compress.src/JFile.java
@@ -0,0 +1,65 @@
+package com.compress.src;
+
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.net.URI;
+
+public class JFile extends File {
+
+    public JFile(String s) {
+        super(s);
+    }
+
+    /**
+     * 重写compareTo方法，使其文件夹优先
+     *
+     * @param file 用于比较的对象
+     * @return -1,0,1分别代表小于、等于、大于
+     */
+    @Override
+    public int compareTo(File file) {
+        int result = 0;
+        if (this.isDirectory() && file.isDirectory()) {
+            result = this.getName().compareTo(file.getName());
+        } else if (this.isDirectory() && file.isFile()) {
+            result = -1;
+        } else if (this.isFile() && file.isFile()) {
+            result = this.getName().compareTo(file.getName());
+        } else {
+            result = 1;
+        }
+        return result;
+    }
+
+    public JFile(String s, String s1) {
+        super(s, s1);
+    }
+
+    public JFile(File file, String s) {
+        super(file, s);
+    }
+
+    public JFile(URI uri) {
+        super(uri);
+    }
+
+    /**
+     * 重写toString方法，使其在Jlist中显示的时候只显示当前的文件名
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        if (this.isFile())
+            return getName();
+        else
+            return "/" + this.getName();
+    }
+
+    public static void main(String[] args) {
+        FileSystemView fsv = FileSystemView.getFileSystemView();
+        File com = fsv.getHomeDirectory();
+        JFile itm = new JFile(com.getPath());
+        System.out.println(itm);
+    }
+}
